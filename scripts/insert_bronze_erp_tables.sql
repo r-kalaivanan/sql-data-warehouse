17eb2e57941f550099b7d7fa9c6c0a37442172ea
r-kalaivanan <24mx108@psgtech.ac.in>
2026-02-21 16:42:30 +0530

    Load data into bronze ERP tables

---
+++ scripts/insert_bronze_erp_tables.sql
@@ -0,0 +1,32 @@
+/*
+
+Truncates the tables in bronze layers and bulk inserts the rows from the tables in ERP source system.
+
+*/
+
+TRUNCATE TABLE bronze.crm_cust_info;
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+TRUNCATE TABLE bronze.crm_prd_info;
+BULK INSERT bronze.crm_prd_info
+FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+TRUNCATE TABLE bronze.crm_sales_details;
+BULK INSERT bronze.crm_sales_details
+FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
