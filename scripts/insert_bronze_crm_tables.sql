3400e4f37bd4f5889764559dab95840cc285a6dd
r-kalaivanan <24mx108@psgtech.ac.in>
2026-02-21 16:53:31 +0530

    Merge the Load logic into a single script and delete the empty insert_bronze_erp_tables.sql

--- scripts/insert_bronze_crm_tables.sql
+++ scripts/insert_bronze_crm_tables.sql
@@ -30,3 +30,30 @@ WITH (
 	FIELDTERMINATOR = ',',
 	TABLOCK
 );
+
+TRUNCATE TABLE bronze.erp_cust_az12;
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+BULK INSERT bronze.erp_loc_a101
+FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
