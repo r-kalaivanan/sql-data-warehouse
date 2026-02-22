f2b999f2a8408f2df7226022e7cf0c4c2f120503
r-kalaivanan <24mx108@psgtech.ac.in>
2026-02-22 15:44:26 +0530

    Update load file with error handling

--- scripts/insert_bronze_crm_tables.sql
+++ scripts/insert_bronze_crm_tables.sql
@@ -6,80 +6,86 @@ Truncates the tables in bronze layers and bulk inserts the rows from the tables 
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	PRINT 'Loading Bronze Layer';
-	PRINT 'Loading CRM Tables';
+	BEGIN TRY 
+		PRINT 'Loading Bronze Layer';
+		PRINT 'Loading CRM Tables';
 
-	PRINT '>> Truncating Table : bronze.crm_cust_info'; 
-	TRUNCATE TABLE bronze.crm_cust_info;
+		PRINT '>> Truncating Table : bronze.crm_cust_info'; 
+		TRUNCATE TABLE bronze.crm_cust_info;
 
-	PRINT '>> Inserting Data into Table : bronze.crm_cust_info';
-	BULK INSERT bronze.crm_cust_info
-	FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT '>> Inserting Data into Table : bronze.crm_cust_info';
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '>> Truncating Table : bronze.crm_prd_info';
-	TRUNCATE TABLE bronze.crm_prd_info;
+		PRINT '>> Truncating Table : bronze.crm_prd_info';
+		TRUNCATE TABLE bronze.crm_prd_info;
 
-	PRINT '>> Inserting Data into Table : bronze.crm_prd_info';
-	BULK INSERT bronze.crm_prd_info
-	FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT '>> Inserting Data into Table : bronze.crm_prd_info';
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '>> Truncating Table : bronze.crm_sales_details';
-	TRUNCATE TABLE bronze.crm_sales_details;
+		PRINT '>> Truncating Table : bronze.crm_sales_details';
+		TRUNCATE TABLE bronze.crm_sales_details;
 
-	PRINT '>> Inserting Data into Table : bronze.crm_sales_details';
-	BULK INSERT bronze.crm_sales_details
-	FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT '>> Inserting Data into Table : bronze.crm_sales_details';
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 	
-	PRINT 'Loading ERP Tables';
+		PRINT 'Loading ERP Tables';
 
-	PRINT '>> Truncating Table : bronze.erp_cust_az12';
-	TRUNCATE TABLE bronze.erp_cust_az12;
+		PRINT '>> Truncating Table : bronze.erp_cust_az12';
+		TRUNCATE TABLE bronze.erp_cust_az12;
 
-	PRINT '>> Inserting Data into Table : bronze.erp_cust_az12';
-	BULK INSERT bronze.erp_cust_az12
-	FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT '>> Inserting Data into Table : bronze.erp_cust_az12';
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '>> Truncating Table : bronze.erp_loc_a101';
-	TRUNCATE TABLE bronze.erp_loc_a101;
+		PRINT '>> Truncating Table : bronze.erp_loc_a101';
+		TRUNCATE TABLE bronze.erp_loc_a101;
 
-	PRINT '>> Inserting Data into Table : bronze.erp_loc_a101';
-	BULK INSERT bronze.erp_loc_a101
-	FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT '>> Inserting Data into Table : bronze.erp_loc_a101';
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '>> Truncating Table : bronze.erp_px_cat_g1v2';
-	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+		PRINT '>> Truncating Table : bronze.erp_px_cat_g1v2';
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 
-	PRINT '>> Inserting Data into Table : bronze.erp_px_cat_g1v2';
-	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT '>> Inserting Data into Table : bronze.erp_px_cat_g1v2';
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\prema\Desktop\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+	END TRY
+	BEGIN CATCH
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+		PRINT 'ERROR MESSAGE' + ERROR_MESSAGE(); 
+	END CATCH
 END;
